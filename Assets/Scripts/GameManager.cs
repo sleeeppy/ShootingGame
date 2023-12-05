6722461937ef826287561145773e5696d50679bc
sleeeppy <sosfbi000@naver.com>
2023-12-05 13:34:12 +0900

    Custom Enemy Placement

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine.VFX;
 
 public class GameManager : MonoBehaviour
 {
     public string[] enemyObjs;
     public Transform[] spawnPoints;
 
-    public float maxSpawnDelay;
+    public float nextSpawnDelay;
     public float curSpawnDelay;
 
     public GameObject player;
@@ -18,18 +21,51 @@ public class GameManager : MonoBehaviour
     public GameObject gameOverSet;
     public ObjectManager objectManager;
 
+    public List<Spawn> spawnList;
+    public int spawni;
+    public bool spawnEnd;
+
     void Awake()
     {
+        spawnList = new List<Spawn>();
         enemyObjs = new string[] { "EnemyS", "EnemyM", "EnemyL" };
+        ReadSpawnFile();
+    }
+
+    void ReadSpawnFile()
+    {
+        spawnList.Clear();
+        spawni = 0;
+        spawnEnd = false;
+
+        TextAsset textFile = Resources.Load("Stage 0") as TextAsset;
+        StringReader stringreader = new StringReader(textFile.text);
+
+        while(stringreader != null)
+        {
+            string line = stringreader.ReadLine();
+            Debug.Log(line);
+
+            if (line == null)
+                break;
+            Spawn spawnData = new Spawn();
+            spawnData.delay = float.Parse(line.Split(',')[0]);
+            spawnData.type = line.Split(',')[1];
+            spawnData.point = int.Parse(line.Split(',')[2]);
+            spawnList.Add(spawnData);
+        }
+
+        stringreader.Close();
+        nextSpawnDelay = spawnList[0].delay;
+
     }
     void Update()
     {
         curSpawnDelay += Time.deltaTime;
         
-        if(curSpawnDelay > maxSpawnDelay)
+        if(curSpawnDelay > nextSpawnDelay && !spawnEnd)
         {
             SpawnEnemy();
-            maxSpawnDelay = Random.Range(0.5f, 3f);
             curSpawnDelay = 0;  
         }
 
@@ -39,22 +75,34 @@ public class GameManager : MonoBehaviour
 
     void SpawnEnemy()
     {
-        int ranEnemy = Random.Range(0, 3);
-        int ranPoint = Random.Range(0, 8);
-        GameObject enemy = objectManager.MakeObj(enemyObjs[ranEnemy]);
-        enemy.transform.position = spawnPoints[ranPoint].position;
+        int enemyi = 0;
+        switch (spawnList[spawni].type)
+        {
+            case "S":
+                enemyi = 0;
+                break;
+            case "M":
+                enemyi = 1;
+                break;
+            case "L":
+                enemyi = 2;
+                break;
+        }
+        int enemyPoint = spawnList[spawni].point;
+        GameObject enemy = objectManager.MakeObj(enemyObjs[enemyi]);
+        enemy.transform.position = spawnPoints[enemyPoint].position;
 
         Rigidbody2D rigid = enemy.GetComponent<Rigidbody2D>();
         Enemy enemyLogic = enemy.GetComponent<Enemy>();
         enemyLogic.player = player;
         enemyLogic.objectManager = objectManager;
 
-        if(ranPoint == 5 || ranPoint == 6)
+        if(enemyPoint == 5 || enemyPoint == 6)
         {
             enemy.transform.Rotate(Vector3.back * 90);
             rigid.velocity = new Vector2(enemyLogic.speed * (-1), -1);
         }
-        else if(ranPoint == 7 || ranPoint == 8)
+        else if(enemyPoint == 7 || enemyPoint == 8)
         {
             enemy.transform.Rotate(Vector3.forward * 90);
             rigid.velocity = new Vector2(enemyLogic.speed, -1);
@@ -63,6 +111,15 @@ public class GameManager : MonoBehaviour
         {
             rigid.velocity = new Vector2 (0, enemyLogic.speed * (-1));
         }
+
+        spawni++;
+        if(spawni == spawnList.Count)
+        {
+            spawnEnd = true;
+            return;
+        }
+
+        nextSpawnDelay = spawnList[spawni].delay;
     }
     public void UpdateLifeIcon(int life)
     {
